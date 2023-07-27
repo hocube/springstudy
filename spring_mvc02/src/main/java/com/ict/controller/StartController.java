53d00528ec998f26afae4e0558f552b289f8d573
jiho <hocube159@gmail.com>
2023-07-27 16:25:55 +0900

    [0727 학원]

---
+++ spring_mvc02/src/main/java/com/ict/controller/StartController.java
@@ -0,0 +1,27 @@
+package com.ict.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class StartController{
+	
+	@GetMapping(value = "/start.do")
+	public ModelAndView exec(HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView mv = new ModelAndView("admin/output");
+		
+		String[] dogName = {"초복이", "중복이", "말복이", "바둑이"};
+		mv.addObject("dogName", dogName);
+		
+		return mv;
+	}
+}
